af7f8165a800dc7b5bcc8509c1d882919467de8a
10xOXR <10xoxr@gmail.com>
2020-01-18 14:28:06 +0000

    Update DATABASE_URL settings

--- blog/settings.py
+++ blog/settings.py
@@ -82,8 +82,7 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# if "DATABASE_URL" in os.environ:
-if development:
+if "DATABASE_URL" in os.environ:
     DATABASES = {
             'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
     }
